66af40a4b439166b694c24e6323f23cda59bdb85
Douglas Lovera <doulovera@gmail.com>
2021-09-23 15:59:13 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import * as express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const initialState = {
+  cart: [],
+  products: [
+    {
+      id: "1",
+      image: "https://arepa.s3.amazonaws.com/camiseta.png",
+      title: "Camiseta",
+      price: 25,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "3",
+      image: "https://arepa.s3.amazonaws.com/mug.png",
+      title: "Mug",
+      price: 10,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "4",
+      image: "https://arepa.s3.amazonaws.com/pin.png",
+      title: "Pin",
+      price: 4,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "5",
+      image: "https://arepa.s3.amazonaws.com/stickers1.png",
+      title: "Stickers",
+      price: 2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "6",
+      image: "https://arepa.s3.amazonaws.com/stickers2.png",
+      title: "Stickers",
+      price: 2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "7",
+      image: "https://arepa.s3.amazonaws.com/hoodie.png",
+      title: "Hoodie",
+      price: 35,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+  ],
+};
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("It's alive in TypeScript!!! jej");
+});
+
+app.get("/api/v1", (req: Request, res: Response) => {
+  res.json(initialState);
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT;
+
+module.exports = {
+  name: "express-server",
+  entry: "./src/index.ts",
+  target: "node",
+  mode: NODE_ENV,
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.ts$/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
+};
